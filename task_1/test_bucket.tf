25a336f8dfaaf2e8b32c27fd45940a20d89c0d39
k_iuferov <ksyuferov@gmail.com>
2025-06-13 22:13:23 +0400

    first commit

---
+++ task_1/test_bucket.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "task-1-bucket-rsschool" {
+  bucket        = "task-1-bucket-rsschool"
+  force_destroy = "true"
+}
+
+resource "aws_s3_bucket_ownership_controls" "task-1-bucket-rsschool" {
+  bucket = aws_s3_bucket.task-1-bucket-rsschool.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "task-1-bucket-rsschool" {
+  depends_on = [aws_s3_bucket_ownership_controls.task-1-bucket-rsschool]
+  bucket = aws_s3_bucket.task-1-bucket-rsschool.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "task-1-bucket-rsschool" {
+  bucket = aws_s3_bucket.task-1-bucket-rsschool.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ task_1/variables.tf
@@ -0,0 +1,4 @@
+variable "test_bucket_name" {
+  description = "The name of the test bucket that will be created"
+  type        = string
+}
